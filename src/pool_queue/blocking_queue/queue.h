0f6709b1046083bb70946447ceba8f77afa08444
Konstantin Morozov <just.morozov.k@gmail.com>
2024-05-10 18:28:45 +0200

    add queue

--- src/pool_queue/blocking_queue/queue.h
+++ src/pool_queue/blocking_queue/queue.h
@@ -4,7 +4,11 @@
 
 #pragma once
 
+#include <atomic>
+#include <condition_variable>
+#include <mutex>
 #include <optional>
+#include <queue>
 
 namespace pool {
 
@@ -12,9 +16,45 @@ namespace pool {
 template <class T>
 class UnboundedBlockingQueue final {
 public:
-    bool Put(T) { return true; }
-    std::optional<T> Take() { return std::nullopt; }
-    void Close() {};
+    bool Put(T value) {
+        if (close_.load()) {
+            return false;
+        }
+
+        std::lock_guard g(m_);
+        data_.push(std::move(value));
+
+        empty_data_.notify_one();
+        return true;
+    }
+
+    std::optional<T> Take() {
+        std::optional<T> result;
+
+        std::unique_lock lock(m_);
+        while (data_.empty() && !close_.load()) {
+            empty_data_.wait(lock);
+        }
+
+        if (!data_.empty()) {
+            result.emplace(std::move(data_.front()));
+            data_.pop();
+        }
+        lock.unlock();
+
+        return result;
+    }
+
+    void Close() {
+        close_.store(true);
+        empty_data_.notify_all();
+    };
+
+private:
+    std::queue<T> data_;
+    std::mutex m_;
+    std::condition_variable empty_data_;
+    std::atomic<bool> close_{false};
 };
 
 }  // namespace pool
