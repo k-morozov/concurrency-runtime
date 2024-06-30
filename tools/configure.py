e63bbcd2bf62639e012b6040a18459c605994994
Konstantin Morozov <just.morozov.k@gmail.com>
2024-06-30 11:24:51 +0200

    up

--- tools/configure.py
+++ tools/configure.py
@@ -5,6 +5,10 @@ commands = [
     "python3 -m venv env",
     "pip install -r requirements.txt",
     "sudo bash -c \"$(wget -O - https://apt.llvm.org/llvm.sh)\"",
+    "sudo rm /usr/bin/c++",
+    "sudo ln -s /usr/bin/clang++-18 /usr/bin/c++",
+    "sudo rm /usr/bin/clang++",
+    "sudo ln -s /usr/bin/clang++-18 /usr/bin/clang++",
     "conan install . -of=buildDir --build=missing -pr:a=./config/debug_profile",
 ]
 
