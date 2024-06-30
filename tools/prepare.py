bf013c7f4c00d4f67d1564ef489ee0d3f75e12f0
Konstantin Morozov <just.morozov.k@gmail.com>
2024-06-30 11:06:02 +0200

    up prepare

--- tools/prepare.py
+++ tools/prepare.py
@@ -15,16 +15,18 @@ import subprocess
 commands = [
     "sudo apt-get install python3 python3-pip python3-setuptools python3-wheel ninja-build",
     "python3 -m venv ./env/",
-    # "source env/bin/activate",
-    "pip3 install --user meson",
+    "source env/bin/activate",
+    "pip install -r requirements.txt",
+    # env/bin/conan install . -of=buildDir --build=missing -pr:a=./config/debug_profile
+    # "pip3 install --user meson",
     # "pip3 install meson",
     "sudo bash -c \"$(wget -O - https://apt.llvm.org/llvm.sh)\"",
     "sudo rm /usr/bin/c++",
     "sudo ln -s /usr/bin/clang++-18 /usr/bin/c++",
     "sudo rm /usr/bin/clang++",
     "sudo ln -s /usr/bin/clang++-18 /usr/bin/clang++",
-    "pip install conan",
-    "conan install . -of=buildDir --build=missing -pr:a=./config/debug_profile",
+    # "pip install conan",
+    "env/bin/conan install . -of=buildDir --build=missing -pr:a=./config/debug_profile",
 ]
 
 for cmd in commands:
