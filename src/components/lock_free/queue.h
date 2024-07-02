c905f9cff8cd63db5cd06d88515b0602bb5ffc12
Konstantin Morozov <just.morozov.k@gmail.com>
2024-07-02 09:21:31 +0200

    in progress

--- src/components/lock_free/queue.h
+++ src/components/lock_free/queue.h
@@ -15,54 +15,91 @@ namespace NComponents {
  */
 template <class T>
 class LockFreeQueue final {
+    struct Node;
+
     struct NodePointer {
-        NodePointer* ptr{};
+        Node* ptr{};
         size_t counter{};
+
+        bool operator==(const NodePointer& rhs) const {
+            return this->ptr == rhs.ptr && this->counter == rhs.counter;
+        }
     };
 
-    using AtomicNodePointer = std::atomic<NodePointer*>;
-
     struct Node {
-        T value;
+        T value{};
         std::atomic<NodePointer> next;
     };
 
 private:
-    AtomicNodePointer head;
-    AtomicNodePointer tail;
+    std::atomic<NodePointer> head;
+    std::atomic<NodePointer> tail;
 
 public:
     LockFreeQueue() {
         Node* dummy = new Node();
-        dummy->next.ptr = nullptr;
-        head = tail = dummy;
+        NodePointer p{dummy, 0};
+        head = p;
+        tail = p;
     }
     ~LockFreeQueue() {
 
     }
 
     void Push(T item) {
-        auto* new_node = new Node();
+        Node* new_node = new Node();
         new_node->value = std::move(item);
-        new_node->next.ptr = nullptr;
+//        new_node->next.ptr = nullptr;
+
+        NodePointer old_tail{};
+        NodePointer old_next{};
 
         while(true) {
-            Node* old_tail = tail.load();
-            Node* next = old_tail->next.load();
+            old_tail = tail.load();
+            old_next = old_tail.ptr->next.load();
 
-            if (tail == tail.load()) {
-                if (next->ptr == nullptr) {
-                    if (tail->next.compare_exchange_strong(tail, {new_node, next + 1})) {
+            if (old_tail == tail.load()) {
+                if (old_next.ptr == nullptr) {
+                    if (tail.load().ptr->next.compare_exchange_strong(old_next, {new_node, old_next.counter + 1})) {
                         break;
                     }
                 } else {
-
+                    tail.compare_exchange_strong(old_tail, {old_next.ptr, old_tail.counter + 1});
                 }
             }
         }
+        tail.compare_exchange_strong(old_tail, {old_next.ptr, old_tail.counter + 1});
     }
 
-    std::optional<T> TryPop();
+    std::optional<T> TryPop() {
+        std::optional<T> result;
+
+//        NodePointer old_head{};
+//        NodePointer old_tail{};
+//
+//        while (true) {
+//            old_head = head.load();
+//            old_tail = tail.load();
+//            NodePointer next = old_head.ptr->next;
+//
+//            if (old_head == head.load()) {
+//                if (old_head.ptr == old_tail.ptr) {
+//                    if (next.ptr == nullptr) {
+//                        return {};
+//                    }
+//                    tail.compare_exchange_strong(old_tail, {next.ptr, old_tail.counter + 1});
+//                } else {
+//                    result.emplace(std::move(next.ptr->value));
+//                    if (head.compare_exchange_strong(old_head, {next.ptr, old_head.counter + 1})) {
+//                        break;
+//                    }
+//                }
+//            }
+//        }
+//
+//        delete old_head.ptr;
+        return result;
+    }
 };
 
 }  // namespace NComponents
---
+++ test/components/test_lk_queue.cpp
@@ -0,0 +1,20 @@
+//
+// Created by konstantin on 02.07.24.
+//
+#include "gtest/gtest.h"
+
+#include <thread>
+
+#include <components/lock_free/queue.h>
+
+TEST(TestLockFreeQueue, JustWorks) {
+    NComponents::LockFreeQueue<std::string> stack;
+
+    stack.Push("Data");
+    auto item = stack.TryPop();
+    ASSERT_TRUE(item);
+    ASSERT_EQ(*item, "Data");
+
+    auto empty = stack.TryPop();
+    ASSERT_FALSE(empty);
+}
